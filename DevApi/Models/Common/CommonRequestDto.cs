651aca4da999804e64b2fbca2828207d3778a0da
Dev00700 <iamdevendra03@outlook.com>
2025-06-23 21:53:35 +0530

    commit

--- DevApi/Models/Common/CommonRequestDto.cs
+++ DevApi/Models/Common/CommonRequestDto.cs
@@ -2,11 +2,13 @@ namespace DevApi.Models.Common
 {
     public class CommonRequestDto<T>
     {
-        public int CompanyId { get; set; }
+        public int? CompanyId { get; set; }
         public int PageSize { get; set; }
         public int PageRecordCount { get; set; }
         public T Data { get; set; } // Accepts any DTO type
 
         public long UserId { get; set; }
+        public Guid MCompanyGuid { get; set; }
+        public Guid CompanyGuid { get; set; }
     }
 }
--- DevApi/Models/Common/CommonResponseDto.cs
+++ DevApi/Models/Common/CommonResponseDto.cs
@@ -7,11 +7,12 @@ namespace DevApi.Models.Common
 {
     public class CommonResponseDto<T>
     {
-        public int CompanyId { get; set; }
+        public int? CompanyId { get; set; }
         public int PageSize { get; set; }
         public int PageRecordCount { get; set; }
         public T Data { get; set; } // Can hold any DTO or list of DTOs
         public int Flag { get; set; }
         public string Message { get; set; }
+
     }
 }
